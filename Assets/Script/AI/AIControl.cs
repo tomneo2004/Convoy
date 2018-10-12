5db9460081b9040f32455e84992a973466f56114
NelsonPunch <tomneo2004@gmail.com>
2018-10-12 14:41:12 +1100

    Flocking AI

--- Assets/Script/AI/AIControl.cs
+++ Assets/Script/AI/AIControl.cs
@@ -29,34 +29,31 @@ namespace Convoy
         // Update is called once per frame
         void Update()
         {
-            Vector2 steer = flockAgent.GetSteer();
-            steer.Normalize();
-            //Vector2 dir = new Vector2(target.transform.position.x, target.transform.position.y) - 
-                //new Vector2(transform.position.x, transform.position.y);
-
-            
-
-            GetComponent<Rigidbody2D>().velocity += steer * speed * 20f * Time.deltaTime;
+            Vector2 steer = flockAgent.GetSteerVelocity();
 
             if (steer != Vector2.zero)
             {
-                Vector2 dir = GetComponent<Rigidbody2D>().velocity;
-                dir.Normalize();
-
-                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-                angle -= 90f;
-                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                //move
+                transform.position += new Vector3(steer.x, steer.y, 0f)*Time.deltaTime;
             }
+            else
+            {
+                Vector2 tDir = target.transform.position - transform.position;
+
+                //move
+                tDir = tDir.normalized * flockAgent.speed * Time.deltaTime;
+                transform.position += new Vector3(tDir.x, tDir.y, 0f);
+            }
+
+            //rotate
+            Vector2 dir = steer;
+            dir.Normalize();
+
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            angle -= 90f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
 
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = Color.red;
-
-            Vector3 vec = GetComponent<Rigidbody2D>().velocity;
-            vec.Normalize();
-            Gizmos.DrawLine(transform.position, transform.position+(vec * 5f));
-        }
     }
 }
 
--- Assets/Script/AI/Flocking/FlockAgent.cs
+++ Assets/Script/AI/Flocking/FlockAgent.cs
@@ -11,22 +11,76 @@ namespace Convoy
         /// </summary>
         public FlockAgentMonitor monitor;
 
+        [Header("GizmoDraw")]
+        public bool drawVelocity = false;
+        public float drawLength = 5f;
+        public Color drawVelocityColor = Color.black;
+
+        public bool drawScanRadius = false;
+        public Color drawScanColor = Color.green;
+
+        public bool drawSeparationRadius = false;
+        public Color drawSeparationColor = Color.red;
+
+        [Header("Flock agent properties")]
         /// <summary>
         /// The raidus of scan for nearby agent
         /// who will be consider as member group
         /// </summary>
+        [Tooltip("Radius for scanning neighbour agent")]
+        [Range(0f, Mathf.Infinity)]
         public float scanRadius = 2f;
 
-        protected Vector2 avgAlignment = Vector2.zero;
-        protected Vector2 avgCohesion = Vector2.zero;
-        protected Vector2 avgSeparation = Vector2.zero;
+        /// <summary>
+        /// Separation radius
+        /// </summary>
+        [Tooltip("Separation radius\n" +
+            "Must be smaller than scan radius")]
+        [Range(0f, Mathf.Infinity)]
+        public float separationRadius = 1f;
 
-        protected Rigidbody2D agentRigidbody;
+        [Tooltip("How much agent can be affected by separation\n")]
+        [Range(0f, Mathf.Infinity)]
+        public float separationWeight = 1f;
+
+        [Tooltip("How much agent can be affected by cohesion")]
+        [Range(0f, Mathf.Infinity)]
+        public float cohesionWeight = 1f;
+
+        [Tooltip("How much agent can be affected by alignment")]
+        [Range(0f, Mathf.Infinity)]
+        public float alignmentWeight = 1f;
+
+
+        /// <summary>
+        /// Velocity of this agent
+        /// </summary>
+        Vector2 velocity = Vector2.zero;
+
+        /// <summary>
+        /// Get velocity of this agent
+        /// </summary>
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+        }
+
+        /// <summary>
+        /// Max speed
+        /// </summary>
+        public float maxSpeed = 1f;
+
+        /// <summary>
+        /// How fast can this agent move
+        /// </summary>
+        public float speed = 1f;
+
 
         // Use this for initialization
         void Start()
         {
-            agentRigidbody = GetComponent<Rigidbody2D>();
+            //float angle = Random.Range(0, Mathf.PI * 2);
+            //velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
         }
 
         // Update is called once per frame
@@ -45,40 +99,72 @@ namespace Convoy
             monitor.Remove(this);
         }
 
-        public Vector2 GetSteer()
+        /// <summary>
+        /// Get steering velocity
+        /// Return flocking velocity
+        /// </summary>
+        /// <returns></returns>
+        public Vector2 GetSteerVelocity()
         {
+            //Get neighbours
             List<FlockAgent> neighbours = monitor.GetNearbyAgent(transform.position, scanRadius, this);
 
             if (neighbours.Count == 0)
-                return Vector2.zero;
+            {
+                velocity = Vector2.zero;
+                return velocity;
+            }
+                
 
-            //CalculateAlignment(neighbours);
-            //CalculateCohesion(neighbours);
-            CalculateSeparation(neighbours);
+            Vector2 alignment = CalculateAlignment(neighbours);
+            Vector2 Cohesion = CalculateCohesion(neighbours);
+            Vector2 separation = CalculateSeparation(neighbours);
 
-            return avgAlignment + avgCohesion + avgSeparation;
+            //calculate amount of acceleration
+            Vector2 avgDir = alignment * alignmentWeight + Cohesion * cohesionWeight + separation * separationWeight;
+            Vector2 acceleration = avgDir * speed;
+
+            //add acceleration to velocity
+            velocity += acceleration;
+
+            //limit velocity
+            if (velocity.sqrMagnitude > maxSpeed * maxSpeed)
+                velocity = velocity.normalized * maxSpeed;
+
+            return velocity;
         }
 
-        void CalculateAlignment(List<FlockAgent> neighbours)
+        /// <summary>
+        /// Calculate alignment and return normalized direction
+        /// </summary>
+        /// <param name="neighbours"></param>
+        /// <returns></returns>
+        Vector2 CalculateAlignment(List<FlockAgent> neighbours)
         {
-            avgAlignment = Vector2.zero;
-
-            if (agentRigidbody == null)
-                return;
+           Vector2 avgAlignment = Vector2.zero;
 
             for(int i=0; i<neighbours.Count; i++)
             {
-                FlockAgent agent = neighbours[i];
+                FlockAgent otherAgent = neighbours[i];
 
-                avgAlignment += agent.GetComponent<Rigidbody2D>().velocity;
+                avgAlignment += otherAgent.Velocity;
             }
 
             avgAlignment /= neighbours.Count;
+
+            avgAlignment.Normalize();
+
+            return avgAlignment;
         }
 
-        void CalculateCohesion(List<FlockAgent> neighbours)
+        /// <summary>
+        /// Calculate cohesion and return normalized direction
+        /// </summary>
+        /// <param name="neighbours"></param>
+        /// <returns></returns>
+        Vector2 CalculateCohesion(List<FlockAgent> neighbours)
         {
-            avgCohesion = Vector2.zero;
+            Vector2 avgCohesion = Vector2.zero;
 
             for (int i = 0; i < neighbours.Count; i++)
             {
@@ -88,33 +174,91 @@ namespace Convoy
                 avgCohesion += agentPos;
             }
 
-            avgCohesion /= neighbours.Count;
-            avgCohesion = avgCohesion - new Vector2(transform.position.x, transform.position.y);
+            if(neighbours.Count > 0)
+            {
+                avgCohesion /= neighbours.Count;
+
+                Vector2 pos = transform.position;
+                Vector2 dir = avgCohesion - pos;
+
+                dir.Normalize();
+
+                return dir;
+            }
+
+            return Vector2.zero;
 
         }
 
-        void CalculateSeparation(List<FlockAgent> neighbours)
+        /// <summary>
+        /// Calcualte separation and return normalized direction
+        /// </summary>
+        /// <param name="neighbours"></param>
+        /// <returns></returns>
+        Vector2 CalculateSeparation(List<FlockAgent> neighbours)
         {
-            avgSeparation = Vector2.zero;
+            Vector2 avgSeparation = Vector2.zero;
+
+            if (neighbours.Count == 0)
+                return avgSeparation;
+
+            int count = 0;
 
             for (int i = 0; i < neighbours.Count; i++)
             {
                 FlockAgent agent = neighbours[i];
 
                 Vector2 agentPos = agent.transform.position;
-                Vector2 diff = new Vector2(transform.position.x, transform.position.y) - agentPos;
-                diff /= diff.sqrMagnitude;
-                avgSeparation += diff;
+                Vector2 diff = agentPos - new Vector2(transform.position.x, transform.position.y);
+                float dist = Vector2.Distance(agentPos, transform.position);
+
+                if(diff.sqrMagnitude > 0f && dist < (separationRadius + agent.separationRadius))
+                {
+                    diff = diff / diff.sqrMagnitude;
+                    avgSeparation += diff;
+
+                    count++;
+                }
+                
             }
 
-            avgSeparation /= neighbours.Count;
+            //inverse vector
+            avgSeparation *= -1f;
+
+            avgSeparation /= count;
+
+            avgSeparation.Normalize();
+
+            return avgSeparation;
         }
 
         private void OnDrawGizmos()
         {
-            Gizmos.color = Color.white;
-            Gizmos.DrawWireSphere(transform.position, scanRadius);
+            
+            if(drawVelocity)
+            {
+                Gizmos.color = drawVelocityColor;
+                Vector3 vec = velocity.normalized ;
+                Gizmos.DrawLine(transform.position, transform.position + (vec * drawLength));
+
+                
+
+                
+            }
+
+            if (drawSeparationRadius)
+            {
+                Gizmos.color = drawSeparationColor;
+                Gizmos.DrawWireSphere(transform.position, separationRadius);
+            }
+
+            if (drawScanRadius)
+            {
+                Gizmos.color = drawScanColor;
+                Gizmos.DrawWireSphere(transform.position, scanRadius);
+            }
         }
+
     }
 }
 
